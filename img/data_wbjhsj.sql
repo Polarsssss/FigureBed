0592d893768a8e989c94a00456159f609a005fa4
IXA丶鳴神 <1055300264@qq.com>
2021-01-07 15:34:23 +0800

    Upload by PicGo

---
+++ img/data_wbjhsj.sql
@@ -0,0 +1,44 @@
+﻿prompt Importing table DM_YHS_ZBDM_WBJHSJ...
+set feedback off
+set define off
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('HBCFXX001', '环保处罚信息', 'reports/adp/yhs/month/hbcfxx/qccx.fpd', 1, '2', 'G', 'Y', 'Y', 'HBCFXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SCGLBMXX001', '工商登记信息', 'reports/adp/yhs/month/scglbmxx/qccx1.fpd', 1, '2', 'G', 'Y', 'Y', 'SCGLBMXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SCGLBMXX002', '工商投资方信息', 'reports/adp/yhs/month/scglbmxx/qccx2.fpd', 2, '2', 'G', 'Y', 'Y', 'SCGLBMXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SCGLBMXX003', '工商投资总额信息', 'reports/adp/yhs/month/scglbmxx/qccx3.fpd', 3, '2', 'G', 'Y', 'Y', 'SCGLBMXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SCGLBMXX004', '工商附行业信息', 'reports/adp/yhs/month/scglbmxx/qccx4.fpd', 4, '2', 'G', 'Y', 'Y', 'SCGLBMXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SCGLBMXX005', '工商注册资本信息', 'reports/adp/yhs/month/scglbmxx/qccx5.fpd', 5, '2', 'G', 'Y', 'Y', 'SCGLBMXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SCGLBMXX006', '工商变更信息', 'reports/adp/yhs/month/scglbmxx/qccx6.fpd', 6, '2', 'G', 'Y', 'Y', 'SCGLBMXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SCGLBMXX007', '工商家庭经营信息', 'reports/adp/yhs/month/scglbmxx/qccx7.fpd', 7, '2', 'G', 'Y', 'Y', 'SCGLBMXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SCGLBMXX008', '工商注销信息', 'reports/adp/yhs/month/scglbmxx/qccx8.fpd', 8, '2', 'G', 'Y', 'Y', 'SCGLBMXX', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('SIFA001', '最高法失信被执行人信息', 'reports/adp/yhs/month/sf/qccx.fpd', 1, '2', 'G', 'Y', 'Y', 'SIFA', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('HAIGUAN001', '海关报关单', 'reports/adp/yhs/month/hg/qccx.fpd', 1, '2', 'G', 'Y', 'Y', 'HAIGUAN', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('WAIHUI001', '外汇企业分类管理信息', 'reports/adp/yhs/month/wh/qccx1.fpd', 1, '2', 'G', 'Y', 'Y', 'WAIHUI', null, '2', null, null);
+
+insert into DM_YHS_ZBDM_WBJHSJ (ZB_DM, ZB_MC, ZB_PATH, ZB_SX, ZBJC, GDSBZ, XYBZ, YXBZ, FZB_DM, ZB_LYB, SDSQBZ, LBLX, ZB_TAB)
+values ('WAIHUI002', '企业货物贸易逐笔出口收汇数据', 'reports/adp/yhs/month/wh/qccx2.fpd', 2, '2', 'G', 'Y', 'Y', 'WAIHUI', null, '2', null, null);
+
+prompt Done.
